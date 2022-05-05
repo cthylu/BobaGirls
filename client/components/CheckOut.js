fe13a3e45e6aebe2aba53ffd967865b3f5bd4939
Kim <wukimberly1@gmail.com>
2022-05-05 19:34:16 -0400

    pushablecheckout

--- client/components/CheckOut.js
+++ client/components/CheckOut.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchCart } from "../store/cart";
 import users from "../store/users";
+import { createShipping } from "../store/checkout";
+import { Link } from "react-router-dom";
 
 class CheckOut extends React.Component {
   constructor() {
@@ -17,11 +20,13 @@ class CheckOut extends React.Component {
     this.checkout = this.checkout.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  async componentDidMount() {
+    this.props.fetchCart();
+  }
   checkout(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
   handleSubmit(e) {
-    e.preventdefault();
     this.props.createShipping({ ...this.state });
     this.setState(
       (this.state = {
@@ -38,10 +43,31 @@ class CheckOut extends React.Component {
   render() {
     const { firstname, lastname, email, address, city, state, zipcode } =
       this.state;
+    const { cart } = this.props;
     const { checkout, handleSubmit } = this;
     return (
       <div>
-        {/* this part would contain everything on cart page with price */}
+        {cart.map((item) => {
+          return (
+            <div key={item.id}>
+              {console.log(item)}
+              {item.isCart === true && item.lineitems.length > 0 ? (
+                <div>
+                  You have {item.lineitems.length} items in your
+                  <Link to={"cart"}> Cart </Link>
+                  <div> Your total is $</div>
+                  <div> To continue checking out, fill out the below. </div>
+                </div>
+              ) : (
+                <div>
+                  No items in your cart! Try some of our
+                  <Link to={"/prodcuts"}> Products! </Link>
+                </div>
+              )}
+            </div>
+          );
+        })}
+
         <form id="checkout" className="checkout" onSubmit={handleSubmit}>
           <label htmlFor="firstname"> First Name*: </label>
           <input
@@ -63,6 +89,7 @@ class CheckOut extends React.Component {
           <br />
           <label htmlFor="email"> Email*: </label>
           <input
+            type="email"
             name="email"
             onChange={checkout}
             value={email}
@@ -72,7 +99,6 @@ class CheckOut extends React.Component {
           <br />
           <label htmlFor="address"> Address*: </label>
           <input
-            type="email"
             name="address"
             onChange={checkout}
             value={address}
@@ -103,22 +129,23 @@ class CheckOut extends React.Component {
             name="zipcode"
             onChange={checkout}
             value={zipcode}
-            placeholder="Zipcode"
+            placeholder="Zip-code"
             required
           />
           <br />
-          <button type="submit"> Submit </button>
+          <button type="submit"> Complete Purchase </button>
         </form>
       </div>
     );
   }
 }
 
-// const mapState = (state) => (state);
+const mapState = (state) => state;
 
 const mapDispatch = (dispatch, { history }) => ({
+  fetchCart: (userId) => dispatch(fetchCart(userId)),
   createShipping: (information) =>
     dispatch(createShipping(information, history)),
 });
 
-export default connect(null, mapDispatch)(CheckOut);
+export default connect(mapState, mapDispatch)(CheckOut);
